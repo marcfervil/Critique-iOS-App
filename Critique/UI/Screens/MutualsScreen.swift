3e762b77022e6cbdb0c388a4d98f22681f90f7d7
FourteenFourHundredStudios <marcfervil630@gmail.com>
2018-06-21 20:45:18 -0500

    Mutual Screen Changes

--- Critique/UI/Screens/MutualsScreen.swift
+++ Critique/UI/Screens/MutualsScreen.swift
@@ -7,14 +7,53 @@
 //
 
 import Foundation
+import UIKit
 
+class Mutuals : UIViewController , UITableViewDataSource, UITableViewDelegate{
+    
+    var text = ["ye","okay","sup"]
+    
+    @IBOutlet weak var MutualsTableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return text.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath) as! UserTableViewCell
+        
+        /*
+        let fruitName = fruits[indexPath.row]
+        cell.label?.text = fruitName
+        cell.fruitImageView?.image = UIImage(named: fruitName)
+        */
+        //cell.imageView?.image = col
+        
+       // cell.imageView?.backgroundColor =  UIColor.red
+        
+        cell.Username.text? = text[indexPath.row]
+        
+        self.MutualsTableView.tableFooterView = UIView()
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
 
-class Mutuals : ViewController {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped cell number \(indexPath.row).")
+    }
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         
         view.tag = 1
-        
+        MutualsTableView.dataSource = self
+        MutualsTableView.delegate = self
     }
     
 }
---
+++ Critique/UI/View Controllers/UserTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  UITableViewCell.swift
+//  Critique
+//
+//  Created by Marc Fervil on 20/6/18.
+//  Copyright © 2018 Marc Fervil. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserTableViewCell : UITableViewCell {
+
+    
+    @IBOutlet weak var Username: UILabel!
+ 
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    
+    
+}
